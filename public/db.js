62f3e2f0817146720082c6a7a85c4e8f3ea12512
MarioCabral <mariocabral.a@gmail.com>
2023-07-04 14:20:12 -0300

    Arrumando a conexão do adm_area

--- public/db.js
+++ public/db.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase
         // Your web app's Firebase configuration
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
         const firebaseConfig = {
-            //aqui você faz a conexão
+            //aqui você faz a conexão com o banco
         };
 
         const app = initializeApp(firebaseConfig);
